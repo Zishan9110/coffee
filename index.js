c89277b954bf3c8a0fc69b3f00122c64a0f1efb2
Zishan9110 <145319858+Zishan9110@users.noreply.github.com>
2023-10-01 20:51:21 +0530

    added index.js

---
+++ index.js
@@ -0,0 +1,9 @@
+function submitForm() {
+    const name = document.getElementById("name").value;
+    const email = document.getElementById("email").value;
+    const phone = document.getElementById("phone").value;
+
+    console.log("Name: " + name);
+    console.log("Email: " + email);
+    console.log("Phone Number: " + phone);
+}
